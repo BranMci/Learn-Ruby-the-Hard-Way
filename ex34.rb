1b087ca659c40361933a8aa2ae98f9c7094d9544
CrixsB <brandon@brandonmcinnis.com>
2013-09-02 21:21:55 +0900

    ex35

---
+++ ex34.rb
@@ -0,0 +1,21 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The first animal is at 0, and is a bear.
+puts animals[0]
+
+# The second animal is at 1, and is a python.
+puts animals[1]
+
+# The third animal is at 2, and is a peacock.
+puts animals[2]
+
+# The fourth animal is at 3, and is a kangaroo.
+puts animals[3]
+
+# The fifth animal is at 4, and is a whale.
+puts animals[4]
+
+# The sixth animal is at 5, and is a platypus.
+puts animals[5]
+
+# Basically, the rule is that if you have an array position, add one to get it's ordinal position. (first, second, third, etc.)
