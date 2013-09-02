c6c3cf82cd4d8b35a56f5f32ee85fb833883034c
CrixsB <brandon@brandonmcinnis.com>
2013-09-02 19:23:31 +0900

    ex33 EC

--- ex33.rb
+++ ex33.rb
@@ -1,18 +1,19 @@
-i = 0
+$i = 0
 numbers = []
 
-def add_to_array (array, max)
-	for i in 0...max
-		puts "At the top i is #{i}"
-		array.push(i)
+def add_to_array (array, inc, max)
+	for n in 0...max
+		puts "At the top i is #{$i}"
+		array.push($i)
 
-		i = i + 1
+		$i = $i + inc
 		puts "Numbers now: #{array}"
-		puts "At the bottom i is #{i}"
+		puts "At the bottom i is #{$i}"
 	end
 end
 
-add_to_array(numbers, 6) 
+# here, we're telling it to run 10 total times, but to increment the value of $i by 2 each time.
+add_to_array(numbers, 2 , 10) 
 
 puts "The numbers: "
 
