6d641ff25820eb2868688fb1698b60d5f7bf66c6
CrixsB <brandon@brandonmcinnis.com>
2013-09-06 01:34:14 +0900

    start ex37 defs

---
+++ ex37.rb
@@ -0,0 +1,8 @@
+# In this exercise, we are asked to explain what several symbols do in Ruby from memory. If we do not know what a given symbol does, we are asked to search for it online, record its definition, and then flashcard it.
+
+# alias: creates an alias for a given variable or method
+	# use: method.alias :new_name :old_name
+
+# and: same as &&
+
+# BEGIN: no idea
