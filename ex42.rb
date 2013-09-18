fcf31cd4f4f41443207eebea5c9f1c89f8ec8819
CrixsB <brandon@brandonmcinnis.com>
2013-09-18 18:50:44 +0900

    Identified relationships (is-a/has-a)

--- ex42.rb
+++ ex42.rb
@@ -1,8 +1,8 @@
-## Animal is-a object Looka t the extra credit
+## Animal is-a object Look at the extra credit
 class Animal
 end
 
-## ??
+## Dog is-an Animal
 class Dog < Animal
   def initialize(name)
     ##  ??
@@ -10,18 +10,18 @@ class Dog < Animal
   end
 end
 
-## ??
+## Cat is-an Animal
 class Cat < Animal
   def initialize(name)
-    ## ??
+    ## Cat has-a name
     @name = name
   end
 end
 
-## ??
+## Person is-an object
 class Person
   def initialize(name)
-    ## ??
+    ## Person has-a name
     @name = name
 
     ## Person has-a pet of some kind
@@ -31,12 +31,12 @@ class Person
   attr_accessor :pet
 end
 
-## ??
+## Employee is-a Person
 class Employee < Person
   def initialize(name, salary)
-    ## ?? hmm what is this strange magic?
+    ## call the initialize function of Person, passing 'name'
     super(name)
-    ## ??
+    ## Employee has-a salary
     @salary = salary
   end
 end
@@ -44,24 +44,24 @@ end
 class Fish
 end
 
-## ??
+## Salmon is-a Fish
 class Salmon < Fish
 end
 
-## ?? 
+## Halibut is-a Fish
 class Halibut < Fish
 end
 
 ## rover is-a Dog
 rover = Dog.new("Rover")
 
-## ??
+## satan is-a Cat (Seriously? Terrible name for a pet.)
 satan = Cat.new("Satan")
 
-## ??
+## mary is a-a Person
 mary = Person.new("Mary")
 
-## ??
+## Mary has-a pet. Satan. (That's some thick irony right there.)
 mary.pet = satan
 
 ## ??
