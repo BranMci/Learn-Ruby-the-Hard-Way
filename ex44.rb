cdbcce5904812725e8c4843fb4496e5eab01a80c
CrixsB <brandon@brandonmcinnis.com>
2013-09-25 18:26:02 +0900

    Composition - ex44

--- ex44.rb
+++ ex44.rb
@@ -1,41 +1,41 @@
-class Parent
+class Other
 
   def override()
-    puts "PARENT override()"
+    puts "OTHER override()"
   end
 
   def implicit()
-    puts "PARENT implicit()"
+    puts "OTHER implicit()"
   end
 
   def altered()
-    puts "PARENT altered()"
+    puts "OTHER altered()"
   end
 end
 
-class Child < Parent
+class Child
+
+  def initialize()
+    @other = Other.new()
+  end
+
+  def implicit()
+    @other.implicit()
+  end
+
   def override()
     puts "CHILD override()"
   end
 
   def altered()
-    puts "CHILD, BEFORE PARENT altered()"
-    super()
-    puts "CHILD, AFTER PARENT altered()"
+    puts "CHILD, BEFORE OTHER altered()"
+    @other.altered()
+    puts "CHILD, AFTER OTHER altered()"
   end
 end
 
-dad = Parent.new()
 son = Child.new()
 
-dad = Parent.new()
-son = Child.new()
-
-dad.implicit()
 son.implicit()
-
-dad.override()
 son.override()
-
-dad.altered()
 son.altered()
