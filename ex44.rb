f4a491d98b0ce3a103da98645f29a87225b8e8cb
CrixsB <brandon@brandonmcinnis.com>
2013-09-25 17:38:13 +0900

    All Three Combined - ex44

--- ex44.rb
+++ ex44.rb
@@ -1,12 +1,23 @@
 class Parent
 
+  def override()
+    puts "PARENT override()"
+  end
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+
   def altered()
     puts "PARENT altered()"
   end
-
 end
 
 class Child < Parent
+  def override()
+    puts "CHILD override()"
+  end
+
   def altered()
     puts "CHILD, BEFORE PARENT altered()"
     super()
@@ -16,6 +27,15 @@ end
 
 dad = Parent.new()
 son = Child.new()
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
 
 dad.altered()
 son.altered()
